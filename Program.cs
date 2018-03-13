a91b46eddd6e31a10c4af280732bda03486bea8c
Jack Hyc <Jacek.Hyc@gmail.com>
2018-03-13 12:17:11 -0500

    adding new repository

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace simpleCrud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
